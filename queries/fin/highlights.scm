33a570c1fd5118b0c00137b143d0e7e0e184bdef
mrmcc3 <mrmcc3@gmail.com>
2024-07-04 15:52:47 +1000

    helix highlights

--- queries/fin/highlights.scm
+++ queries/fin/highlights.scm
@@ -1,14 +1,9 @@
 (ERROR) @error
 (com) @comment
 (sym) @variable
-(num) @number @constant.numeric
-(esc) @string @string.quoted.double
-(esc_seq) @escape @constant.character.escape
-(raw) @string @string.raw
+(num) @number
+(esc) @string.quoted.double
+(esc_seq) @escape
+(raw) @string.raw
 ["," "="] @punctuation.delimiter
 ["(" ")" "[" "]"] @punctuation.bracket
-
-;; TODO
-;; colons within symbols should highlight as @punctuation.delimiter
-;; symbols that tag collections could highlight different?
-;; symbols that are extensions could highlight different?
